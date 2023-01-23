077fc8a5b19256fbc8aa27cd8c908352cc982f24
LucileSD <4316@holbertonschool.com>
2023-01-23 09:59:30 +0100

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        string representation of a float
+        args:
+            n: the number
+        return the string
+    """
+    return str(n)
