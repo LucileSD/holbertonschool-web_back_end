4686b633bcf30ee437272e032ee4d8a519f0494c
LucileSD <4316@holbertonschool.com>
2023-01-23 09:55:45 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Basic annotations - floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+        floor of a number
+        args:
+            n: the number
+        return the floor of this number
+    """
+    return math.floor(n)
