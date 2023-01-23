5edc764a4bd07a87255cbb3592a922b1dc20bb95
LucileSD <4316@holbertonschool.com>
2023-01-23 09:48:52 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        concatenation of 2 strings
+        args:
+            str1: the first string
+            str2: the second string
+        return the new string
+    """
+    return str1 + str2
