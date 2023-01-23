a45b52c6189c9cbc60d3fbce7d57e391e9af5e29
LucileSD <4316@holbertonschool.com>
2023-01-23 09:44:31 +0100

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        function add 2 float
+        args:
+            a: the first number
+            b the second number
+        return the sum of the 2 numbers
+    """
+    return a + b
