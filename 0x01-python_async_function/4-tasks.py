5cf7f5d1118a35dc934bae8d557e5813998ec07c
LucileSD <4316@holbertonschool.com>
2023-01-24 12:35:44 +0100

    try

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,7 +6,8 @@
 from typing import List
 import asyncio
 
-task_wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
