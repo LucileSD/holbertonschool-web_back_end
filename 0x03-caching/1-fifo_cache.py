a84d49ae58a2a46683fe0842b3f4fca1109a790c
LucileSD <4316@holbertonschool.com>
2023-01-26 17:41:35 +0100

    FIFO caching

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+    Basic dictionary
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+         FIFO caching
+    """
+    def __init__(self):
+        """
+            initiate a new array
+        """
+        super().__init__()
+        self.key_order = []
+
+    def put(self, key, item):
+        """
+            assign the item to the dictinary key or delete a key
+            args:
+                key: the key of the dictionary
+                item: the value of the key
+        """
+        if key is not None or item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                discard_key = self.key_order.pop(0)
+                self.cache_data.pop(discard_key)
+                print("DISCARD: {}".format(discard_key))
+            self.cache_data[key] = item
+            self.key_order.append(key)
+
+    def get(self, key):
+        """
+            get the value of a key
+            args:
+                key: the key in the dictionary
+            return:
+                the value of the key or none
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
