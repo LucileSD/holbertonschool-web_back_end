81db19e379fd0fca9c3a79f5bbff10ebe8157b3a
LucileSD <4316@holbertonschool.com>
2023-01-26 18:43:05 +0100

    MRU LRU

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+    Basic dictionary
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+        LRU Caching
+    """
+    def __init__(self):
+        """
+            initiate a new array
+        """
+        super().__init__()
+        self.recently_used = []
+
+    def put(self, key, item):
+        """
+            assign the item to the dictinary key or delete a key
+            args:
+                key: the key of the dictionary
+                item: the value of the key
+        """
+        if key is not None or item is not None:
+            if key in self.cache_data:
+                self.recently_used.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                discarded = self.recently_used.pop(0)
+                del self.cache_data[discarded]
+                print("DISCARD: {}".format(discarded))
+        self.cache_data[key] = item
+        self.recently_used.append(key)
+
+    def get(self, key):
+        """
+            get the value of a key
+            args:
+                key: the key in the dictionary
+            return:
+                the value of the key or none
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            self.recently_used.remove(key)
+            self.recently_used.append(key)
+            return self.cache_data[key]
---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+    Basic dictionary
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+        MRU Caching
+    """
+    def __init__(self):
+        """
+            initiate a new array
+        """
+        super().__init__()
+        self.recently_used = []
+
+    def put(self, key, item):
+        """
+            assign the item to the dictinary key or delete a key
+            args:
+                key: the key of the dictionary
+                item: the value of the key
+        """
+        if key is not None or item is not None:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.recently_used.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                discard_key = self.recently_used.pop(-1)
+                del self.cache_data[discard_key]
+                print("DISCARD: {}".format(discard_key))
+        self.cache_data[key] = item
+        self.recently_used.append(key)
+
+    def get(self, key):
+        """
+            get the value of a key
+            args:
+                key: the key in the dictionary
+            return:
+                the value of the key or none
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            self.recently_used.remove(key)
+            self.recently_used.append(key)
+            return self.cache_data[key]
