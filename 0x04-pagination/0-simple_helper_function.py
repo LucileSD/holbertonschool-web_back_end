b7f28ba63e7f61c14a51148b1a82c5f5041da7a0
LucileSD <4316@holbertonschool.com>
2023-01-30 10:07:01 +0100

    Simple helper function

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    Simple helper function
+"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+        pagination helper
+        return a tuple of start index and end index
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
