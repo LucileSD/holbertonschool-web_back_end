ca31af80ed9b69dc5757fa43218c7ec3e7ffe6c0
LucileSD <4316@holbertonschool.com>
2023-02-01 16:08:34 +0100

    check valid password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -6,10 +6,21 @@
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """
         Encrypting passwords
     """
     pwd = password.encode('utf-8')
     hashed = bcrypt.hashpw(pwd, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        Check valid password
+    """
+    pwd = password.encode('utf-8')
+    if bcrypt.checkpw(pwd, hashed_password):
+        return True
+    else:
+        return False
