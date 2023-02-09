a7a15b27514b98d7b79efddb8a1e9cac748d22a5
LucileSD <4316@holbertonschool.com>
2023-02-09 10:19:19 +0100

    pep8

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -27,6 +27,7 @@ if os.getenv("AUTH_TYPE") == 'session_exp_auth':
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
 
+
 @app.before_request
 def before_request():
     """check authentification before request"""
