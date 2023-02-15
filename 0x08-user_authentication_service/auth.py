856f0a341ef6105796f5ac44281ba2eb1adda61e
LucileSD <4316@holbertonschool.com>
2023-02-15 12:41:36 +0100

    fix register user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -34,7 +34,8 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError("User {} already exists".format(email))
         except NoResultFound:
-            self._db.add_user(email, _hash_password(password))
+            user = self._db.add_user(email, _hash_password(password))
+            return user
 
     def valid_login(self, email, password) -> bool:
         """check if password match with the hashed password"""
