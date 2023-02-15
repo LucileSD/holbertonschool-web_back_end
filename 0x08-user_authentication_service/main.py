53cb050249316d76f2f7f72f208451b0e6d47ee9
LucileSD <4316@holbertonschool.com>
2023-02-15 10:30:56 +0100

    pycodestyle

--- 0x08-user_authentication_service/main.py
+++ 0x08-user_authentication_service/main.py
@@ -6,48 +6,64 @@ import requests
 
 
 def register_user(email: str, password: str) -> None:
-    """"""
-    data={"email": email, "password": password}
+    """test new user is registered"""
+    data = {"email": email, "password": password}
     response = requests.post("http://localhost:5000/users",
                              data=data)
-    assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
+    assert response.status_code == 200
 
 
 def log_in_wrong_password(email: str, password: str) -> None:
+    """test with a wrong password"""
     data = {"email": email, "password": password}
     response = requests.post("http://localhost:5000/sessions", data=data)
-    assert response.status_code == 401, f"Expected status code 401, got {response.status_code}"
+    assert response.status_code == 401
+
 
 def log_in(email: str, password: str) -> str:
+    """test login"""
     data = {"email": email, "password": password}
     response = requests.post("http://localhost:5000/sessions", data=data)
-    assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
+    assert response.status_code == 200
     return response.cookies.get("session_id")
 
+
 def profile_unlogged() -> None:
+    """test a wrong profile"""
     response = requests.get("http://localhost:5000/profile")
-    assert response.status_code == 403, f"Expected status code 403, got {response.status_code}"
+    assert response.status_code == 403
+
 
 def profile_logged(session_id: str) -> None:
+    """test with a good profile"""
     headers = {"session_id": session_id}
     response = requests.get("http://localhost:5000/profile", cookies=headers)
-    assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
+    assert response.status_code == 200
+
 
 def log_out(session_id: str) -> None:
+    """test logout"""
     headers = {"session_id": session_id}
-    response = requests.delete("http://localhost:5000/sessions", cookies=headers)
-    assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
+    response = requests.delete("http://localhost:5000/sessions",
+                               cookies=headers)
+    assert response.status_code == 200
+
 
 def reset_password_token(email: str) -> str:
+    """test reset_token"""
     data = {"email": email}
     response = requests.post("http://localhost:5000/reset_password", data=data)
-    assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
+    assert response.status_code == 200
     return response.json()["reset_token"]
 
+
 def update_password(email: str, reset_token: str, new_password: str) -> None:
-    data = {"email": email, "reset_token": reset_token, "new_password": new_password}
+    """test change password"""
+    data = {"email": email, "reset_token": reset_token,
+            "new_password": new_password}
     response = requests.put("http://localhost:5000/reset_password", data=data)
-    assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
+    assert response.status_code == 200
+
 
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
@@ -65,4 +81,3 @@ if __name__ == "__main__":
     reset_token = reset_password_token(EMAIL)
     update_password(EMAIL, reset_token, NEW_PASSWD)
     log_in(EMAIL, NEW_PASSWD)
-
