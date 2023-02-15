bd1af94e5d7e54899ac6a7250bfe1ef90a9c8f18
LucileSD <4316@holbertonschool.com>
2023-02-15 15:00:59 +0100

    fix

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -30,8 +30,8 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """add a user in the database"""
         try:
-            user = self._db.find_user_by(email=email)
-            raise ValueError("User {} already exists".format(user.email))
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
 
@@ -41,7 +41,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(password.encode('utf-8'),
                                   user.hashed_password)
-        except NoResultFound:
+        except Exception:
             return False
 
     def create_session(self, email: str) -> str:
--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -2,7 +2,6 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
