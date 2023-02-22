326c10b2f18cedb981838d786fe2a3d6036eebd8
LucileSD <4316@holbertonschool.com>
2023-02-22 12:47:20 +0100

    Mocking a property

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -19,3 +19,13 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org)
         self.assertEqual(client.org, patched_get_json.return_value)
         patched_get_json.assert_called_once()
+
+    def test_public_repos_url(self):
+        """test public_repos()"""
+        org = "google"
+        patched = "client.GithubOrgClient.org"
+        expected = "https://api.github.com/orgs/google/repos"
+        payload = {"repos_url": expected}
+        with patch(patched, PropertyMock(return_value=payload)):
+            obj = GithubOrgClient(org)
+            self.assertEqual(obj._public_repos_url, expected)
