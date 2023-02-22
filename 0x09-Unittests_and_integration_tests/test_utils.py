a478f024be94feda1d36cc5a8abaf252a1b8acfe
LucileSD <4316@holbertonschool.com>
2023-02-22 11:24:55 +0100

    Parameterize a unit test raise

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -18,3 +18,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """test method access_nested_map()"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a", ), "a"),
+        ({"a": 1}, ("a", "b"), 1)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """test error for access_nested_map()"""
+        with self.assertRaises(KeyError) as raises:
+            access_nested_map(nested_map, path)
+            self.assertEqual(raises.exception, expected)
