21b2bcb509e85fd1b709e3e19a8481431d587dd2
LucileSD <4316@holbertonschool.com>
2023-02-23 13:21:16 +0100

    Parametrize templates

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""module application"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """configuration language"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+babel.init_app(app)
+
+
+@app.route('/')
+def hello():
+    """print hello world"""
+    title = _("home_title")
+    h1 = _("home_header")
+    return (render_template("3-index.html", title=title, hello=h1))
+
+
+@babel.localeselector
+def get_locale():
+    """define the best language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
