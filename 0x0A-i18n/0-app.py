3534b632074fc54640be951e4fe039c440a73033
LucileSD <4316@holbertonschool.com>
2023-02-23 11:21:06 +0100

    Basic Flask app

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""module application"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """print hello world"""
+    title = "Welcome to Holberton"
+    h1 = "Hello world"
+    return render_template("0-index.html", title=title, hello=h1)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
