6d3078c531959058cc8d9f3af44ee34935ad176c
LucileSD <4316@holbertonschool.com>
2023-03-07 10:44:12 +0100

    fix pep8

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -66,7 +66,7 @@ class Cache():
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable]= None) ->\
+    def get(self, key: str, fn: Optional[Callable] = None) ->\
             Union[str, bytes, int, float]:
         """convert the data back to the desired format"""
         if fn:
