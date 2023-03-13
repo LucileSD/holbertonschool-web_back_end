9174b8a139f20962f65167538ab370f8ebfcfd04
LucileSD <4316@holbertonschool.com>
2023-03-13 12:41:13 +0100

    log stats with loop

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -9,19 +9,13 @@ collection = client.logs.nginx
 count = collection.count_documents({})
 
 print("{} logs".format(count))
-
-count_get = collection.count_documents({"method": "GET"})
-count_post = collection.count_documents({"method": "POST"})
-count_put = collection.count_documents({"method": "PUT"})
-count_patch = collection.count_documents({"method": "PATCH"})
-count_del = collection.count_documents({"method": "DELETE"})
-
 print("Methods:")
-print("    method GET: {}".format(count_get))
-print("    method POST: {}".format(count_post))
-print("    method PUT: {}".format(count_put))
-print("    method PATCH: {}".format(count_patch))
-print("    method DELETE: {}".format(count_del))
+
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+for method in methods:
+    print("    method {}: {}".format(method,
+                                     collection.count_documents
+                                     ({"method": method})))
 
 count_status = collection.count_documents({"method": "GET", "path": "/status"})
 print("{} status check".format(count_status))
