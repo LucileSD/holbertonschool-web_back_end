7ebf4c0e321bb37a83724ca4790beb972e5dc005
LucileSD <4316@holbertonschool.com>
2023-03-14 10:36:12 +0100

    no annotations

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """module to insert in a document"""
 
-from pymongo.collection import Collection
 
-
-def insert_school(mongo_collection: Collection, **kwargs):
+def insert_school(mongo_collection, **kwargs):
     """insert a new document in the collection based on kwargs"""
     return mongo_collection.insert_one({**kwargs}).inserted_id
