f3bc8b3c356a361c8889951ef28856dac87d2bd1
LucileSD <4316@holbertonschool.com>
2023-03-14 10:34:21 +0100

    no annotation

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 """module to list documents"""
 
-from pymongo.collection import Collection
-from typing import List
 
-
-def list_all(mongo_collection: Collection) -> List:
+def list_all(mongo_collection):
     """list all documents in a collection"""
     if mongo_collection:
         return mongo_collection.find()
