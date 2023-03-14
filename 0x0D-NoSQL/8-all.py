d07af8213e299e80ee8faae302941089a8e3e845
LucileSD <4316@holbertonschool.com>
2023-03-14 10:33:23 +0100

    main

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -4,8 +4,9 @@
 from pymongo.collection import Collection
 from typing import List
 
-if __name__ == "__main__":
 
-    def list_all(mongo_collection: Collection) -> List:
-        """list all documents in a collection"""
-        return list(mongo_collection.find())
+def list_all(mongo_collection: Collection) -> List:
+    """list all documents in a collection"""
+    if mongo_collection:
+        return mongo_collection.find()
+    return []
