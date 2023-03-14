720a31ffd9d3956aea3e42a6148180107d638551
LucileSD <4316@holbertonschool.com>
2023-03-14 10:38:06 +0100

    no annotations

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 """module to change topics"""
 
-from pymongo.collection import Collection
-from typing import List
 
-
-def update_topics(mongo_collection: Collection, name: str, topics: List):
+def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
     mongo_collection.update_one({'name': name}, {'$set': {'topics': topics}})
