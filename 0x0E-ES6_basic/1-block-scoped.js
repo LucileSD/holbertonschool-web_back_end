fb4cfe198ac0429c9056751045b1b2b305eddbdd
LucileSD <4316@holbertonschool.com>
2023-03-16 11:22:51 +0100

    fix

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
-// no use var
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+    // no use var
     const task = true;
+    // no use var
     const task2 = false;
   }
 
