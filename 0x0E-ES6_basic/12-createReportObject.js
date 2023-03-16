7d8f9d7c6d42a2cc1cb929a665e066e83fc533ff
LucileSD <4316@holbertonschool.com>
2023-03-16 11:53:23 +0100

    ES6 method property syntax

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,6 +1,5 @@
 export default function createReportObject(employeesList) {
   return {allEmployees: employeesList,
-          getNumberOfDepartments: (employees) => {
-            return Object.keys(employees).length;
-          }};
+          getNumberOfDepartments: (employees) => Object.keys(employees).length,
+        };
 }
