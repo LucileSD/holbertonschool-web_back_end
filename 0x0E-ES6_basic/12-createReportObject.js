de7d2a7ce086f71a8492cc11ec157165162733d3
LucileSD <4316@holbertonschool.com>
2023-03-16 11:50:23 +0100

    just ;

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -2,6 +2,6 @@ export default function createReportObject(employeesList) {
   return {allEmployees: employeesList,
           getNumberOfDepartments: (employees) => {
             return Object.keys(employees).length;
-          }}
+          }};
   
 }
