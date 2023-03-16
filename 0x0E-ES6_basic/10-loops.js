05c96ae28f083f43b4878b992b3ce600126179e6
LucileSD <4316@holbertonschool.com>
2023-03-16 11:40:25 +0100

    other way to concatenate

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   let newArray = []
   for (let idx of array) {
-    newArray.push(appendString + idx);
+    newArray.push(`${appendString}${idx}`);
   }
 
   return newArray;
