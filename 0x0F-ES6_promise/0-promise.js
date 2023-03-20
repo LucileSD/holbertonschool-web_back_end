50786a958a8f6426264d041dabe6c9aa5fe61761
LucileSD <4316@holbertonschool.com>
2023-03-20 11:35:47 +0100

    first promises

---
+++ 0x0F-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
+}
+// getResponseFromAPI() is returning a Promise object, which is an instance of the Promise class.
+// it still returns a Promise instance
+// When getResponseFromAPI() is called and returns the Promise, the instanceof operator checks whether the returned value is an instance of the Promise class
