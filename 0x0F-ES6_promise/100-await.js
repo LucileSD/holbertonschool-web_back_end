51ac4d63438dc91fdeb362134fce8321b0cd9999
LucileSD <4316@holbertonschool.com>
2023-03-20 15:33:27 +0100

    Await / Async

---
+++ 0x0F-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function asyncUploadUser() {
+  let obj = {};
+  try {
+    obj = {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+  }
+  catch(err) {
+    obj = {
+      photo: null,
+      user: null,
+    };
+  }
+  return obj;
+}
