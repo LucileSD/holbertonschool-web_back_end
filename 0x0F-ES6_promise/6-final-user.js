6abe0a3e04603ba3972654468aa86c0d88b7f758
LucileSD <4316@holbertonschool.com>
2023-03-20 14:39:03 +0100

    Handle multiple promises

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const arrayOfPromise = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ]
+  return Promise.allSettled(arrayOfPromise).then((values) => values)
+}
