1f8f5fa175865c1d1623ef94c2fd2f30bc37d0ac
LucileSD <4316@holbertonschool.com>
2023-03-21 10:03:39 +0100

    comment

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,4 +1,3 @@
-// Don't make a promise...if you know you can't keep it
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
@@ -6,5 +5,5 @@ export default function getFullResponseFromAPI(success) {
     } else {
       reject(new Error('The fake API is not working currently'));
     }
-    });
+  });
 }
