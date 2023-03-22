77ce208ecc1a4ead16e2199863325e21e0df08b1
LucileSD <4316@holbertonschool.com>
2023-03-22 11:43:48 +0100

    getter setter

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== "string") {
+      throw new TypeError("Name must be a string");
+    }
+    if (typeof length !== "number") {
+      throw new TypeError("Length must be a number");
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError("Students must be an array");
+    }
+    this._name = name;
+    this._lenght = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof(name) !== "string") {
+      throw new TypeError(`Name must be a string`);
+    }
+    this._name = name;
+  }
+
+  get lenght() {
+    return this._lenght;
+  }
+
+  set lenght(lenght) {
+    if (typeof(lenght) !== "number") {
+      throw new TypeError(`Lenght must be a number`);
+    }
+    this._lenght = lenght;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError(`Students must be an array`);
+    }
+    this._students = students;
+  }
+}
