838b9d03e1b62133caed1aede36cc98bb76c7039
LucileSD <4316@holbertonschool.com>
2023-03-27 15:56:40 +0200

    throw

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,13 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  try {
-    if (position < length) {
-      const buffer = new ArrayBuffer(length);
-      const array = new Int8Array(buffer);
-      array[position] = value;
-      return new DataView(buffer);
-    }
-    throw new Error('Position outside range');
-  } catch (error) {
-    return error.message;
+  if (position < length) {
+    const buffer = new ArrayBuffer(length);
+    const array = new Int8Array(buffer);
+    array[position] = value;
+    return new DataView(buffer);
   }
+  throw new Error('Position outside range');
 }
