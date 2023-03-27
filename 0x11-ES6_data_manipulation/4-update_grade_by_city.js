4c12b05fdec518814fe91ac2a6e5273f3e2a780e
LucileSD <4316@holbertonschool.com>
2023-03-27 11:58:08 +0200

    combine filter and map

---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+
+      const updateStudent = student;
+      if (gradeObj) {
+        updateStudent.grade = gradeObj.grade;
+      } else {
+        updateStudent.grade = 'N/A';
+      }
+
+      return updateStudent;
+    });
+}
