20eb14928cfb680b03aa27b2b5f081cbc34efab1
LucileSD <4316@holbertonschool.com>
2023-03-28 10:12:46 +0200

    array

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,12 @@
 export default function cleanSet(set, startString) {
-  let str = '';
+  let str = [];
   if (startString === '' || startString === undefined) {
     return '';
   }
   for (const element of set) {
     if (typeof element === 'string' && element.startsWith(startString) === true) {
-      if (str === '') {
-        str += element.slice(startString.length);
-      } else {
-        str = `${str}-${element.slice(startString.length)}`;
-      }
+      str.push(element.slice(startString.length));
     }
   }
-  return str;
+  return str.join('-');
 }
