e3bef75010084ecef36afaad45fc43a4c6a1f7a6
LucileSD <4316@holbertonschool.com>
2023-03-30 12:01:33 +0200

    .then

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -5,19 +5,20 @@ const countStudents = require('./3-read_file_async');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const server = app.createServer(async (req, res) => {
+const server = app.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    try {
-      const content = await countStudents(process.argv[2]);
-      res.end(`${content.join('\n')}`);
-    } catch (err) {
-      res.end(err.message);
-    }
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(`${data.join('\n')}`);
+      })
+      .catch ((err) => {
+        res.end(err.message);
+      })
   }
 });
 
