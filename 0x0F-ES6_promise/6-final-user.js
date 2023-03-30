cb32129d8850e9cf72cd6980d712cdb234730257
LucileSD <4316@holbertonschool.com>
2023-03-30 12:37:29 +0200

    update

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,8 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const arrayOfPromise = [];
+export default /*async */function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser();
+  const photo = uploadPhoto();
+
+  return Promise.allSettled([user, photo]).then((values) => {
+    values.map((v) => ({ status: v.status, value: v.status === 'fulfilled' ? v.value : `${v.reason.name}: ${v.reason.message}` }))});
+  
+  
+  /*const arrayOfPromise = [];
   try {
     const user = await signUpUser(firstName, lastName);
     arrayOfPromise.push({ status: 'fulfilled', value: user });
@@ -10,5 +17,6 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   } catch (error) {
     arrayOfPromise.push({ status: 'rejected', value: error.toString() });
   }
-  return arrayOfPromise;
+  return arrayOfPromise;*/
 }
+
