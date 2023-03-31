594d362acecaea09040091ef15fd113461538bc2
LucileSD <4316@holbertonschool.com>
2023-03-31 13:55:21 +0200

    ok good output

--- 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -5,13 +5,15 @@ class StudentsController {
     response.write('This is the list of our students\n');
     readDatabase('../database.csv')
       .then((data) => {
-        for (let idx = 0; idx < data.length; idx ++) {
-          if (idx === data.length - 2) {
-            response.write(`Number of students in ${data[idx]}: ${data[idx + 1].length}. List: ${data[idx + 1]}`);
+        for (const idx in data) {
+          if (data[idx] !== 'SWE' && data[idx] !== 'CS') {
+            continue;
+          } else if (Number(idx) === data.length - 2) {
+            response.write(`Number of students in ${data[idx]}: ${data[Number(idx) + 1].length}. List: ${data[Number(idx) + 1]}`);
           } else {
-            response.write(`Number of students in ${data[idx]}: ${data[idx + 1].length}. List: ${data[idx + 1]}\n`);
+            response.write(`Number of students in ${data[idx]}: ${data[Number(idx) + 1].length}. List: ${data[Number(idx) + 1]}\n`);
           }
-          idx++;
+          Number(idx) + 1;
         }
         response.status(200).end();
       })
@@ -22,13 +24,16 @@ class StudentsController {
 
   static getAllStudentsByMajor(request, response) {
     const { major } = request.params;
-    console.log(typeof major);
-    if (major !== 'SWE' || major !== 'CS') {
+    if (major !== 'SWE' && major !== 'CS') {
       response.status(500).send('Major parameter must be CS or SWE');
     } else {
       readDatabase('../database.csv')
         .then((data) => {
-          response.status(200).send(`${data.join('\n')}`);
+          for (const idx in data) {
+            if (data[idx] === major) {
+              response.status(200).send(`List: ${data[Number(idx) + 1]}`);
+            }
+          }
         })
         .catch((err) => {
           response.status(500).send('Cannot load the database');
