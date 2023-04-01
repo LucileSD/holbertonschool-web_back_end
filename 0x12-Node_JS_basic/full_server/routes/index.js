2c2b470aae72c9fb7e8210a47f81af518ec72ec0
LucileSD <4316@holbertonschool.com>
2023-04-01 14:29:21 +0200

    db

--- 0x12-Node_JS_basic/full_server/routes/index.js
+++ 0x12-Node_JS_basic/full_server/routes/index.js
@@ -8,11 +8,11 @@ const router = express.Router();
 router.get('/', AppController.getHomepage);
 
 router.get('/students', (req, res) => {
-  StudentsController.getAllStudents(req, req, process.argv[2])
+  StudentsController.getAllStudents(req, res, process.argv[2]);
 });
 
-router.get('/students/:major', (req, req) => {
-  StudentsController.getAllStudentsByMajor(req, res, process.argv[2])
+router.get('/students/:major', (req, res) => {
+  StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
 });
 
 export default router;
