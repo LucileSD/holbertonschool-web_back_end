4ce2f129be4af3d853d0f01fcb80ceb62bf11d4c
LucileSD <4316@holbertonschool.com>
2023-04-01 13:45:45 +0200

    writeHead

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -3,7 +3,6 @@ const app = require('http');
 const port = 1245;
 
 const server = app.createServer((req, res) => {
-  res.statusCode = 200;
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
