0f6d4b016296cdc6579fbddaefae8b0372500716
LucileSD <4316@holbertonschool.com>
2023-04-05 18:05:32 +0200

    one more test

--- 0x13-unittests_in_js/10-api/api.test.js
+++ 0x13-unittests_in_js/10-api/api.test.js
@@ -6,55 +6,55 @@ const api = require('./api');
 
 describe('test for index page', () => {
   describe('test for status code and body', () => {
-    it('should send the status code 200, Welcome to the payment system', () => new Promise((done) => {
+    it('should send the status code 200, Welcome to the payment system', () => (done) => {
       const url = 'http://localhost:7865';
       request(url, (err, res, body) => {
         expect(res.statusCode).to.equal(200);
         expect(body).to.equal('Welcome to the payment system');
         done();
       });
-    }));
+    });
   });
   describe('test for status code and body with url cart', () => {
-    it('should send status code 200 if id is a number', () => new Promise((done) => {
+    it('should send status code 200 if id is a number', () => (done) => {
       const url = 'http://localhost:7865/cart/12';
       request(url, (err, res, body) => {
         expect(res.statusCode).to.equal(200);
         expect(body).to.equal('Payment methods for cart 12');
         done();
       });
-    }));
+    });
   });
   describe('test for status code with url cart with no number', () => {
-    it('should send status code 404 if no id', () => new Promise((done) => {
+    it('should send status code 404 if no id', () => (done) => {
       const url = 'http://localhost:7865/cart/';
       request(url, (err, res, body) => {
         expect(res.statusCode).to.equal(404);
         done();
       });
-    }));
+    });
   });
   describe('test for status code with url cart with letter', () => {
-    it('should send status code 404 if id is a word', () => new Promise((done) => {
+    it('should send status code 404 if id is a word', () => (done) => {
       const url = 'http://localhost:7865/cart/hello';
       request(url, (err, res, body) => {
         expect(res.statusCode).to.equal(404);
         done();
       });
-    }));
+    });
   });
   describe('test /available_payments available_payments', () => {
-    it('should send status code 200 and {"payment_methods":{"credit_cards":true,"paypal":false}}', () => new Promise((done) => {
+    it('should send status code 200 and {"payment_methods":{"credit_cards":true,"paypal":false}}', () => (done) => {
       const url = 'http://localhost:7865/available_payments';
       request(url, (err, res, body) => {
         expect(res.statusCode).to.deep.equal(200);
         expect(body).to.deep.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
         done();
       });
-    }));
+    });
   });
   describe('test /login available_payments', () => {
-    it('should send status code 200 and Welcome Betty', () => new Promise((done) => {
+    it('should send status code 200 and Welcome Betty', () => (done) => {
       const option = {
         url: 'http://localhost:7865/login',
         method: 'POST',
@@ -65,10 +65,10 @@ describe('test for index page', () => {
         expect(body).to.deep.equal('Welcome Betty');
         done();
       });
-    }));
+    });
   });
   describe('test /login available_payments', () => {
-    it('should send status code 200 and Welcome undefined', () => new Promise((done) => {
+    it('should send status code 200 and Welcome undefined', () => (done) => {
       const option = {
         url: 'http://localhost:7865/login',
         method: 'POST',
@@ -78,6 +78,6 @@ describe('test for index page', () => {
         expect(body).to.deep.equal('Welcome undefined');
         done();
       });
-    }));
+    });
   });
 });
