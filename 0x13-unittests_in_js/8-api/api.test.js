a40778adc39352ec1b8abb9c2c21603d3e6e4972
LucileSD <4316@holbertonschool.com>
2023-04-05 17:01:56 +0200

    divide test

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -6,10 +6,17 @@ const request = require('request');
 describe('test for index page', () => {
   describe('test for status code and body', () => {
     const url = 'http://localhost:7865';
-    it('should send the status code 200, Welcome to the payment system', () => {
+    it('should display Welcome to the payment system', () => {
+      request(url, (err, res, body) => {
+        expect(body).to.equal('Welcome to the payment system');
+      });
+    });
+  });
+  describe('test for status code and body', () => {
+    const url = 'http://localhost:7865';
+    it('should send the status code 200', () => {
       request(url, (err, res, body) => {
         expect(res.statusCode).to.equal(200);
-        expect(body).to.equal('Welcome to the payment system');
       });
     });
   });
