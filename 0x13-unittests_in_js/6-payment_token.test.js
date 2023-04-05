1e58eca59e33532bdf1cf195e598d9aa742c3136
LucileSD <4316@holbertonschool.com>
2023-04-05 17:43:26 +0200

    ok function

--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -5,7 +5,7 @@ const expect = chai.expect;
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should display Successful response from the API if true is passed', () => (done) => {
+  it('should display Successful response from the API if true is passed', (done) => {
     getPaymentTokenFromAPI(true)
       .then((response) => {
         expect(response).to.deep.equal({ data: 'Successful response from the API' });
