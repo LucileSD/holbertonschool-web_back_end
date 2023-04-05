9751b4d52179ba6072372fffa7670f872e429039
LucileSD <4316@holbertonschool.com>
2023-04-05 16:49:39 +0200

    success

--- 0x13-unittests_in_js/6-payment_token.js
+++ 0x13-unittests_in_js/6-payment_token.js
@@ -1,7 +1,9 @@
-module.exports = function getPaymentTokenFromAPI(success) {
-  if (success === true) {
+function getPaymentTokenFromAPI(success) {
+  if (success) {
     return new Promise((resolve, reject) => {
       resolve({ data: 'Successful response from the API' });
     });
   }
-};
+}
+
+module.exports = getPaymentTokenFromAPI;
