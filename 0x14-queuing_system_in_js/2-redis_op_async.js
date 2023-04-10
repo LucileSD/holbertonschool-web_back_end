11498d1af41fe00f760eebc189a5a647729c0233
LucileSD <4316@holbertonschool.com>
2023-04-10 15:53:53 +0200

    error.message

--- 0x14-queuing_system_in_js/2-redis_op_async.js
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -5,7 +5,7 @@ const client = createClient();
 const getAsync = promisify(client.get).bind(client);
 
 client.on('connect', () => console.log('Redis client connected to the server'));
-client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
+client.on('error', (err) => console.log(`Redis client not connected to the server: ${err.message}`));
 
 const setNewSchool = (schoolName, value) => {
   client.set(schoolName, value, print);
