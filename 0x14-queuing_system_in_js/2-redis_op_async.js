d1835187c9b91cad4bd8969af574ae402b24b2aa
LucileSD <4316@holbertonschool.com>
2023-04-10 15:50:24 +0200

    eslint

--- 0x14-queuing_system_in_js/2-redis_op_async.js
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -5,21 +5,18 @@ const client = createClient();
 const getAsync = promisify(client.get).bind(client);
 
 client.on('connect', () => console.log('Redis client connected to the server'));
-client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
+client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
 
 const setNewSchool = (schoolName, value) => {
   client.set(schoolName, value, print);
 };
 
-const displaySchoolValue = (schoolName) => {
-  return getAsync(schoolName).then((res) => {
-    console.log(res);
-  });
-};
+const displaySchoolValue = (schoolName) => getAsync(schoolName).then((res) => {
+  console.log(res);
+});
 
 (async () => {
   await displaySchoolValue('Holberton');
   setNewSchool('HolbertonSanFrancisco', '100');
   await displaySchoolValue('HolbertonSanFrancisco');
 })();
-
