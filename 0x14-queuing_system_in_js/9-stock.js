0515347b5e74ba6dd8bfb2fa4ab90e79d3bb1580
LucileSD <4316@holbertonschool.com>
2023-04-13 11:02:07 +0200

    try

--- 0x14-queuing_system_in_js/9-stock.js
+++ 0x14-queuing_system_in_js/9-stock.js
@@ -43,7 +43,7 @@ const reserveStockById = (itemId, stock) => {
 
 const getCurrentReservedStockById = async (itemId) => {
   const stock = await getAsync(`item.${itemId}`);
-  return Number(stock);
+  return stock;
 };
 
 app.get('/list_products', async (req, res) => {
@@ -53,23 +53,31 @@ app.get('/list_products', async (req, res) => {
 app.get('/list_products/:itemId', async (req, res) => {
   const item = getItemById(Number(req.params.itemId));
   const stock = await getCurrentReservedStockById(Number(req.params.id));
+  console.log('list: ' + stock);
   if (!item) {
     res.json({ status: 'Product not found' });
   } else {
-    res.json({ ...item, currentQuantity: item.initialAvailableQuantity - stock });
+    stock = stock === null ? item.initialAvailableQuantity : stock;
+    item.currentQuantity = stock;
+    res.json({ item });
   }
 });
 
 app.get('/reserve_product/:itemId', async (req, res) => {
   const item = getItemById(Number(req.params.itemId));
-  const stock = Number(await getCurrentReservedStockById(Number(req.params.id)));
+  const stock = await getCurrentReservedStockById(Number(req.params.id));
+  console.log(stock);
   if (!item) {
     res.json({ status: 'Product not found' });
   } else {
-    if (item.initialAvailableQuantity - stock === 0) {
+    if (stock === null) {
+      stock = item.initialAvailableQuantity;
+    }
+    if (stock < 1) {
       res.json({ status:"Not enough stock available", itemId: `${item.itemId}` });
     } else {
-      reserveStockById(item.itemId, stock + 1);
+      reserveStockById(item.itemId, stock - 1);
+      console.log(stock);
       res.json({ status:"Reservation confirmed", itemId: `${item.itemId}` });
     }
   }
